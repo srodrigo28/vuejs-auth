b112b3b63799b890105c23aeca9dc2123a9ce256
srodrigo28 <rodrigoexer1@gmail.com>
2022-01-21 18:19:17 -0300

    first commit

---
+++ front/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>Login</h1>
+    <form @submit.prevent="efetuarLogin">
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input type="email" class="form-control" v-model="usuario.email" />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input type="password" class="form-control" v-model="usuario.senha" />
+      </div>
+      <button type="submit" class="btn btn-primary brn-block">Logar</button>
+      <router-link :to="{ name: 'novo.usuario' }">Não possui um cadastro, cadastre-se aqui!</router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {}
+    };
+  },
+  methods: {
+    efetuarLogin() {
+      this.$store
+        .dispatch("efetuarLogin", this.usuario)
+        .then(() => this.$router.push({ name: "gerentes" }));
+    }
+  }
+};
+</script>
